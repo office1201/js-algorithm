7bad4c4b7496b0f48772bc3b19234f498d4c7cce
taehyunKim <office.div1201@gmail.com>
2021-05-12 00:45:34 +0900

    Fix: 6. 최소값 구하기 에러 코드 수정

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
 "use strict";
 
-const solution = (arr) => {
-  const reduceArr = arr.reduce((num1, num2) => num1 + num2, 0);
-  for (let i = 0; i <= arr.length; i++) {
-    const element = arr[i];
-    for (let j = 1; j <= arr.length; i++) {
-      const element1 = arr[i];
-      if (reduceArr - (element + element1) === 100) {
-        arr.splice(i, 1);
-        arr.splice(j, 1);
-      }
-    }
-  }
-  return arr;
+const solution = (...num) => {
+  const minNum = Math.min(...num);
+  return minNum;
 };
 
-console.log(solution([20, 7, 23, 19, 10, 15, 25, 8, 13]));
+console.log(solution(5, 3, 7, 11, 2, 15, 17));
