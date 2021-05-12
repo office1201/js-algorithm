10c512292a0ca68d03a26a75a89b02b1264e6590
taehyunKim <office.div1201@gmail.com>
2021-05-12 13:37:36 +0900

    Add: 11. 대문자 찾기

--- README.md
+++ README.md
@@ -221,6 +221,7 @@ console.log(solution(str));
 
 ```jsx
 // 10. 문자 찾기
+
 const solution = () => {
   let count = 0;
   for (const i of str) {
@@ -243,4 +244,35 @@ const solution = (str, x) => {
 const str = "0x01";
 const x = "x";
 console.log(solution(str, x));
+```
+
+```jsx
+// 11. 대문자 찾기
+
+const solution = (str) => {
+  let result = "";
+  for (let i of str) {
+    if (i === i.toUpperCase()) result += i.toLowerCase();
+    if (i === i.toLowerCase()) result += i.toUpperCase();
+  }
+  return result;
+};
+
+const str = "StuDy";
+console.log(solution(str));
+
+// 2. 생각2-내장함수
+// #charCodeAt()
+// 대문자: 65~90, 소문자: 97~122
+const solution = (str) => {
+  let count = 0;
+  for (let i of str) {
+    let num = i.charCodeAt();
+    if (num >= 65 && num <= 90) count++;
+  }
+  return count;
+};
+
+const str = "StuDy";
+console.log(solution(str));
 ```
--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 "use strict";
 
-// 한 개의 문자열을 입력받고,
-// 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램 작성
+const solution = (str) => {
+  let result = "";
+  for (let i of str) {
+    if (i === i.toUpperCase()) result += i.toLowerCase();
+    if (i === i.toLowerCase()) result += i.toUpperCase();
+  }
+  return result;
+};
 
-const solution = () => {
+const str = "StuDy";
+console.log(solution(str));
+
+// 2. 생각2-내장함수
+// #charCodeAt()
+// 대문자: 65~90, 소문자: 97~122
+const solution = (str) => {
   let count = 0;
-  for (const i of str) {
-    if (i === x) count++;
+  for (let i of str) {
+    let num = i.charCodeAt();
+    if (num >= 65 && num <= 90) count++;
   }
   return count;
 };
 
-let str = "0x0x";
-let x = "x";
-console.log(solution(str, x));
-
-// 2. 생각2-내장함수
-// #split()
-const solution = (str, x) => {
-  const answer = str.split(x).length - 1;
-  return answer;
-};
-
-const str = "0x01";
-const x = "x";
-console.log(solution(str, x));
+const str = "StuDy";
+console.log(solution(str));
