5b457413916ee5c508bcad2915b996173d859450
taehyunKim <office.div1201@gmail.com>
2021-05-13 10:12:18 +0900

    Add: 14. 가장 긴 문자열

--- README.md
+++ README.md
@@ -322,3 +322,26 @@ String.fromCharCode;
 // 2. 생각2-내장함수
 // String.fromCharCode() 고려하기
 ```
+
+```jsx
+// 14. 가장 긴 문자열
+
+const solution = () => {
+  let number;
+  let string;
+  for (const n of N) {
+    if (3 <= n && n <= 30) number = n;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[0].length < arr[i].length) string = arr[i];
+  }
+  return `${number} ${string}`;
+};
+
+const N = [1, 5];
+const arr = ["teacher", "time", "student", "beautiful", "good"];
+console.log(solution(N, arr));
+
+// 2. 생각2-내장함수
+// 없음
+```
--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 "use strict";
 
-// 12. 대문자로 통일
+// 14. 가장 긴 문자열
 
-const solution = (param, param2) => {
-  const result = param2.toUpperCase();
-  return `${param} ${result}`;
+const solution = () => {
+  let number;
+  let string;
+  for (const n of N) {
+    if (3 <= n && n <= 30) number = n;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[0].length < arr[i].length) string = arr[i];
+  }
+  return `${number} ${string}`;
 };
 
-const str = `ItisTimeToStudy`;
-console.log(solution(1, str));
-String.fromCharCode;
+const N = [1, 5];
+const arr = ["teacher", "time", "student", "beautiful", "good"];
+console.log(solution(N, arr));
 
 // 2. 생각2-내장함수
-// String.fromCharCode() 고려하기
+// 없음
