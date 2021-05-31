2c6709cb6fdcbe55c568072e93be10ee685f857a
taehyunKim <office.div1201@gmail.com>
2021-05-31 10:20:06 +0900

    Add: 35. 두 배열 합치기

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 "use strict";
 
-// 34. K번째 큰 수
+// 35. 두 배열 합치기
+const solution = (a, b) => {
+  const arrN = [].concat(a, b);
+  return arrN.sort((a, b) => a - b);
+};
+
+const arr1 = [1, 3, 5];
+const arr2 = [2, 3, 6, 7, 9];
+console.log(solution(arr1, arr2));
