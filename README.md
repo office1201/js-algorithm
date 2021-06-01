3e73e62709cf60c1585c39cbb6d02e20c7702218
taehyunKim <office.div1201@gmail.com>
2021-06-02 07:22:56 +0900

    Add: 연속 부분수열2

--- README.md
+++ README.md
@@ -762,3 +762,24 @@ const solution = (m, arr) => {
 
 console.log(solution(6, a));
 ```
+
+```jsx
+// 38. 연속 부분수혈2
+
+const solution = (m, arr) => {
+  let answer = 0;
+  let sum = 0;
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    while (sum > m) {
+      sum = arr[i++];
+    }
+    answer += i - j + 1;
+  }
+  return answer;
+};
+
+const arr = [1, 3, 1, 2, 3];
+console.log(solution(5, arr));
+```
--- main.js
+++ main.js
@@ -1,31 +1,20 @@
 "use strict";
 
-// 37. 연속 부분수혈1
-const a = [1, 2, 1, 3, 1, 1, 1, 2];
+// 38. 연속 부분수혈2
 
 const solution = (m, arr) => {
   let answer = 0;
-  let i = 0;
   let sum = 0;
-  let index = 0;
-  while (i < arr.length) {
-    if (index === arr.length) {
-      i++;
-      index = i;
-      sum = 0;
-      continue;
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    while (sum > m) {
+      sum = arr[i++];
     }
-
-    sum += arr[index++];
-
-    if (sum === m) {
-      answer++;
-      i++;
-      index = i;
-      sum = 0;
-    }
+    answer += i - j + 1;
   }
   return answer;
 };
 
-console.log(solution(6, a));
+const arr = [1, 3, 1, 2, 3];
+console.log(solution(5, arr));
